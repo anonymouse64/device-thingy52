8c7aa7344b368c79a23d89c44a85e7547f1aefcc
Ian Johnson <ian.johnson@canonical.com>
2019-06-26 22:48:38 -0500

    many: simplify error handling, put BLE addr in Protocols, rm Bootstrap
    * Add methods to the Thingy52Driver to register a device inside the
      Driver
    * Refactor the Thingy52Driver to not have a single device in it and not
      initialize the BLE device before calling Start. Instead we call Start
      in a background goroutine and block the Initialize ProtocolDriver
      method until all of the devices are registered independently.
    * Create a Service object directly and call Start in a background
      goroutine
    * Simplify the error handling to a single cleanup goroutine with a
      single error channel and another goroutine for handling os.Interrupt.
      This also means that we don't need to pass anything to run() when we
      create the BLE device
    * Remove usage of Bootstrap and manually add command line parsing
    * Add BLE map to the Protocols map which contains the Address for the
      device in the DeviceList
    *
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,75 +1,63 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"strconv"
+	"syscall"
 	"time"
 
 	devicethingy52 "github.com/anonymouse64/device-thingy52"
 	"github.com/anonymouse64/device-thingy52/internal/driver"
-	"github.com/edgexfoundry/device-sdk-go/pkg/startup"
+	device "github.com/edgexfoundry/device-sdk-go"
 	"github.com/muka/go-bluetooth/api"
 	"github.com/muka/go-bluetooth/emitter"
 	"github.com/pkg/errors"
 )
 
 const (
-	version     = devicethingy52.Version
-	serviceName = "device-thingy52"
-	// TODO: make these things configurable
-	debug                   = true
+	version                 = devicethingy52.Version
+	serviceName             = "device-thingy52"
 	defaultBluetoothAdapter = "hci0"
-	thingy52BLEAddress      = "EB:B8:28:5B:6A:38"
+)
+
+var (
+	confProfile string
+	confDir     string
+	useRegistry string
+	debug       bool
 )
 
 // run will performs the main actions with the device, setting up backgrond
 // goroutines that continue running after run() returns
-func run(cleanupDone chan struct{}, signalChan chan os.Signal) (*api.Device, error) {
-
-	// setup an interrupt handler to disconnect from the device properly
-	// if we get interrupted
-	devCleanUpChan := make(chan *api.Device, 1)
-	go func() {
-		log.Println("waiting for os signal")
-		<-signalChan
-		log.Println("Received an interrupt, disconnecting device...")
-		// try an immediate read on the channel, if it's empty or closed
-		// do nothing
-		select {
-		case dev, gotValue := <-devCleanUpChan:
-			if gotValue {
-				dev.Disconnect()
-			}
-		default:
-		}
-
-		close(cleanupDone)
-	}()
+func run(addr, adapterName string) (*api.Device, error) {
 
 	if debug {
 		log.Println("setup cleanup handler")
 	}
 
 	// make sure the adapter exists
-	ok, err := api.AdapterExists(defaultBluetoothAdapter)
+	ok, err := api.AdapterExists(adapterName)
 	if err != nil {
-		return nil, errors.Wrapf(err, "checking adapter %s failed", defaultBluetoothAdapter)
+		return nil, errors.Wrapf(err, "checking adapter %s failed", adapterName)
 	} else if !ok {
-		return nil, fmt.Errorf("adapter %s doesn't exist", defaultBluetoothAdapter)
+		return nil, fmt.Errorf("adapter %s doesn't exist", adapterName)
 	}
 
 	// try to turn on the adapter
-	adapter, err := api.GetAdapter(defaultBluetoothAdapter)
+	adapter, err := api.GetAdapter(adapterName)
 	if err != nil {
-		return nil, errors.Wrapf(err, "getting adapter %s failed", defaultBluetoothAdapter)
+		return nil, errors.Wrapf(err, "getting adapter %s failed", adapterName)
 	} else if adapter == nil {
-		return nil, fmt.Errorf("adapter %s is nil", defaultBluetoothAdapter)
+		return nil, fmt.Errorf("adapter for %s is nil", adapterName)
 	}
 
 	if debug {
-		log.Println("got adapter", defaultBluetoothAdapter)
+		log.Println("got adapter for", adapterName)
 	}
 
 	// ensure the adapter is powered
@@ -83,17 +71,17 @@ func run(cleanupDone chan struct{}, signalChan chan os.Signal) (*api.Device, err
 			if len(errRes) > 0 {
 				err = errRes[len(errRes)-1]
 			}
-			return nil, errors.Wrapf(err, "turning adapter %s on failed", defaultBluetoothAdapter)
+			return nil, errors.Wrapf(err, "turning adapter %s on failed", adapterName)
 		}
 	}
 
 	if debug {
-		log.Println("ensured adapter is powered on")
+		log.Printf("ensured adapter %s is powered on\n", adapterName)
 	}
 
 	// start discovery on the adapter
 	if err := adapter.StartDiscovery(); err != nil {
-		return nil, errors.Wrap(err, "starting discovery on adapter %s on failed")
+		return nil, errors.Wrapf(err, "starting discovery on adapter %s on failed", adapterName)
 	}
 
 	if debug {
@@ -104,7 +92,7 @@ func run(cleanupDone chan struct{}, signalChan chan os.Signal) (*api.Device, err
 	devChan := make(chan *api.Device, 1)
 	errRes, err := driver.RetryFunc(100, 100*time.Millisecond, func() error {
 		var err error
-		dev, err := api.GetDeviceByAddress(thingy52BLEAddress)
+		dev, err := api.GetDeviceByAddress(addr)
 		if err != nil {
 			return err
 		} else if dev == nil {
@@ -119,15 +107,12 @@ func run(cleanupDone chan struct{}, signalChan chan os.Signal) (*api.Device, err
 		if len(errRes) > 0 {
 			err = errRes[len(errRes)-1]
 		}
-		return nil, errors.Wrapf(err, "connecting to device with address %s failed", thingy52BLEAddress)
+		return nil, errors.Wrapf(err, "connecting to device with address %s failed", addr)
 	}
 
 	// get the device from the buffered send on the channel from the retry
 	// function
 	dev := <-devChan
-
-	// send the device to the cleanup channel in the background
-	devCleanUpChan <- dev
 
 	if debug {
 		log.Printf("created device handle at %p\n", dev)
@@ -138,6 +123,7 @@ func run(cleanupDone chan struct{}, signalChan chan os.Signal) (*api.Device, err
 		if !dev.IsConnected() {
 			err := dev.Connect()
 			if err != nil {
+				log.Println("failed to connect", err)
 				return err
 			}
 		}
@@ -202,28 +188,156 @@ func run(cleanupDone chan struct{}, signalChan chan os.Signal) (*api.Device, err
 }
 
 func main() {
-	// try to connect to the device before starting the device service proper
+	flag.StringVar(&useRegistry, "registry", "", "Indicates the service should use the registry and provide the registry url.")
+	flag.StringVar(&useRegistry, "r", "", "Indicates the service should use registry and provide the registry path.")
+	flag.StringVar(&confProfile, "profile", "", "Specify a profile other than default.")
+	flag.StringVar(&confProfile, "p", "", "Specify a profile other than default.")
+	flag.StringVar(&confDir, "confdir", "", "Specify an alternate configuration directory.")
+	flag.StringVar(&confDir, "c", "", "Specify an alternate configuration directory.")
+	flag.Parse()
 
 	// setup channels for handling interrupts and safely exiting,
 	// disconnecting from the device before exiting
 	cleanupDone := make(chan struct{})
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
+	errChan := make(chan error)
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	// setup an interrupt handler to disconnect from the device properly
+	// if we get interrupted
+	svcCleanUpChan := make(chan *device.Service, 1)
+	go func() {
+		// wait for an error
+		err := <-errChan
+		// try an immediate read on the service cleanup channel, if it's empty
+		// or closed then do nothing
+		select {
+		case svc, gotValue := <-svcCleanUpChan:
+			if gotValue {
+				// stop the service in a safe way
+				svc.Stop(false)
+			}
+		default:
+		}
+
+		// print the error message and close the cleanup channel
+		// TODO: use edgex logging facilities here
+		log.Println(err)
+
+		// close the cleanupDone channel so the main goroutine exits
+		close(cleanupDone)
+	}()
 
 	go func() {
-		bleDev, err := run(cleanupDone, signalChan)
+		// make a new driver and a dev service out of the driver
+		d := driver.NewThingy52Driver(errChan)
+		svc, err := device.NewService(serviceName, version, confProfile, confDir, useRegistry, d)
 		if err != nil {
-			// if we failed to run properly, log the error and trigger cleanup
-			log.Print(err)
-			signalChan <- os.Interrupt
+			// don't need to send the svc before here, because the error
+			// goroutine doesn't block waiting for the service and will
+			// finish without receiving a service to stop
+			errChan <- err
 			return
 		}
-		d := driver.Thingy52Driver{
-			Dev:           bleDev,
-			InterruptChan: signalChan,
+
+		// send the service on the buffered service cleanup channel so it will
+		// be stopped if hit any critical errors on the errChan
+		svcCleanUpChan <- svc
+
+		// handle debug configuration
+		config := device.DriverConfigs()
+		if debugConfig, ok := config["Debug"]; ok {
+			debug, err = strconv.ParseBool(debugConfig)
+			if err != nil {
+				errChan <- errors.Wrap(err, "\"Debug\" setting in configuration.toml is invalid")
+				return
+			}
 		}
-		// start the device service in the background
-		startup.Bootstrap(serviceName, version, &d)
+
+		// start the device service in order to import the devices from the
+		// configuration.toml file
+		// do this in the background because we need Start to load the devices
+		// from the configuration.toml file, but we also want it to be blocked
+		// until we have a chance to actually setup the connections for
+		// those devices
+		go func() {
+			err := svc.Start(errChan)
+			if err != nil {
+				errChan <- err
+				return
+			}
+		}()
+
+		// wait for the driver to have it's initialization started
+		err = d.WaitForInitStarted(context.TODO())
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		// get all the devices from the service and register them all with
+		// the driver
+		for _, dev := range svc.Devices() {
+			if debug {
+				log.Println("setting up dev", dev.Name)
+			}
+
+			// for each device, get the protocols and the BLE address
+			// in order to actually make a BLE connection to the device
+			devProto := dev.Protocols
+			if devProto == nil {
+				errChan <- errors.Errorf("no \"Protocols\" specified in configuration.toml for device %s", dev.Name)
+				return
+			}
+			devProps, ok := devProto["BLE"]
+			if !ok {
+				errChan <- errors.Errorf("no \"BLE\" key in \"Protocols\" map in configuration.toml for device %s", dev.Name)
+				return
+			}
+			addr, ok := devProps["Address"]
+			if !ok {
+				errChan <- errors.Errorf("no \"Address\" key in \"BLE\" map in configuration.toml for device %s", dev.Name)
+				return
+			}
+
+			// if no adapter was specified, use the default
+			adapterName, ok := devProps["Adapter"]
+			if !ok {
+				adapterName = defaultBluetoothAdapter
+			}
+
+			// try to setup the adapter and connect to the device at the
+			// address
+			bleDev, err := run(addr, adapterName)
+			if err != nil {
+				// if we failed to run properly, log the error and trigger cleanup
+				errChan <- err
+				return
+			}
+
+			// register the device with the driver
+			err = d.RegisterBLEDevice(dev.Name, bleDev)
+			if err != nil {
+				errChan <- err
+				return
+			}
+		}
+
+		// inform the driver we are done registering devices and it can
+		// continue with initialization and starting
+		err = d.FinishedRegisteringDevices(context.TODO())
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		// at this point the goroutine for svc.Start should return
+		// and this routine should return leaving just the error checking
+		// goroutine, the driver's goroutines, the device SDK's goroutines
+		// and the main goroutine here left running
 	}()
 
 	// wait for the cleanup to finish
--- internal/driver/thingy52driver.go
+++ internal/driver/thingy52driver.go
@@ -10,10 +10,8 @@ import (
 	"context"
 	"encoding/binary"
 	"fmt"
-	"log"
 	"math"
 	"math/big"
-	"os"
 	"sync"
 	"time"
 
@@ -24,58 +22,142 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Thingy52Driver is a driver for a thingy52
-type Thingy52Driver struct {
-	lc            logger.LoggingClient
+type thingy52Dev struct {
+	bleDev        *api.Device
 	readingsQueue []thingy52Heading
 	readingCond   *sync.Cond
-	Dev           *api.Device
-	InterruptChan chan os.Signal
 }
 
-type thingy52Heading struct {
-	heading   float32
-	timestamp int64
+// Thingy52Driver is a driver for a thingy52
+type Thingy52Driver struct {
+	lc                  logger.LoggingClient
+	devMap              map[string]*thingy52Dev
+	errChan             chan error
+	initChan            chan struct{}
+	devRegistrationDone chan struct{}
 }
 
-// DisconnectDevice disconnects the device
-func (d *Thingy52Driver) DisconnectDevice(addr *contract.Addressable) error {
-	d.lc.Info(fmt.Sprintf("Thingy52Driver.DisconnectDevice: device-thingy52 driver at %v is disconnecting", addr.Address))
-	return d.Dev.Disconnect()
+// NewThingy52Driver creates a new ProtocolDriver with the associated
+// error channel
+func NewThingy52Driver(errChan chan error) *Thingy52Driver {
+	devMap := make(map[string]*thingy52Dev)
+	initChan := make(chan struct{})
+	devRegistrationDone := make(chan struct{})
+	return &Thingy52Driver{
+		devMap:              devMap,
+		errChan:             errChan,
+		initChan:            initChan,
+		devRegistrationDone: devRegistrationDone,
+	}
 }
 
-// Initialize will start running a background go routine which gathers the data
-// and pushes the data onto a queue which is then read from in the HandleReadCommands
-// initialize expects the device in the driver to already be connected
-func (d *Thingy52Driver) Initialize(lc logger.LoggingClient, asyncCh chan<- *dsModels.AsyncValues) error {
-	d.lc = lc
+// WaitForInitStarted will block until d.Initialize() is called
+func (d *Thingy52Driver) WaitForInitStarted(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return errors.Wrap(ctx.Err(), "waiting for Initialize interrupted")
+	case <-d.initChan:
+		return nil
+	}
+
+	return nil
+}
+
+// FinishedRegisteringDevices will send a signal that devices are done being
+// registered with the driver and Initialize can finish
+func (d *Thingy52Driver) FinishedRegisteringDevices(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return errors.Wrap(
+			ctx.Err(),
+			"finishing registering devices interrupted",
+		)
+	case d.devRegistrationDone <- struct{}{}:
+		return nil
+	}
+
+	return nil
+}
+
+// waitForDevicesRegistered waits until the registration is done
+func (d *Thingy52Driver) waitForDevicesRegistered(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return errors.Wrap(
+			ctx.Err(),
+			"waiting for devices registered interrupted",
+		)
+	case <-d.devRegistrationDone:
+		return nil
+	}
+
+	return nil
+}
+
+// RegisterBLEDevice adds a device to the Thingy52Driver and starts running a
+// background goroutine which gathers the data and pushes the data onto a
+// queue which is then read from in the HandleReadCommands
+// RegisterBLEDevice expects the device to already have been connected and
+// ready to be read from
+// must be called after Initialize has been started, but before
+// FinishedRegisteringDevices is called
+func (d *Thingy52Driver) RegisterBLEDevice(
+	devName string,
+	dev *api.Device,
+) error {
+	// TODO: fail here if this is called before Initialize has started or
+	// after FinishedRegisteringDevices has been called
+
+	// setup the caching queue for this device
 	m := &sync.Mutex{}
-	d.readingCond = sync.NewCond(m)
-	d.readingsQueue = make([]thingy52Heading, 0)
+	readingCond := sync.NewCond(m)
+	readingsQueue := make([]thingy52Heading, 0)
 
+	d.devMap[devName] = &thingy52Dev{
+		bleDev:        dev,
+		readingsQueue: readingsQueue,
+		readingCond:   readingCond,
+	}
+
+	// get the motion characteristic UUID
 	motionUUID := nordicUUID(motionHeadingCharUUID)
+
+	// TODO: check that the device is actually connected before attempting to
+	// register a notify callback
 
 	// attempt to register for notifications for the heading characteristic
 	// from the motion service
 	errRes, err := RetryFunc(100, 100*time.Millisecond, func() error {
-		return registerNotifyCallback(context.TODO(), d.Dev, motionUUID, func(b []byte) {
-			// the heading is returned in fixed point Q16.16 (aka 16Q16 as
-			// Nordic docs refer to ir) format, so first create an exact
-			// integer from the full bytes and make a rational number dividing
-			// by 2^16
-			// see https://en.wikipedia.org/wiki/Q_(number_format)#Q_to_float
-			headingFloat, _ := big.NewRat(int64(binary.LittleEndian.Uint32(b)), 1<<16).Float32()
+		return registerNotifyCallback(
+			context.TODO(),
+			dev,
+			motionUUID,
+			func(b []byte) {
+				// the heading is returned in fixed point Q16.16 (aka 16Q16 as
+				// Nordic docs refer to it) format, so first create an exact
+				// integer from the full bytes and make a rational number
+				// dividing by 2^16
+				// see https://en.wikipedia.org/wiki/Q_(number_format)#Q_to_float
+				headingFloat, _ := big.NewRat(
+					int64(binary.LittleEndian.Uint32(b)),
+					1<<16,
+				).Float32()
 
-			log.Println("heading:", headingFloat)
-			d.readingCond.L.Lock()
-			d.readingsQueue = append(d.readingsQueue, thingy52Heading{
-				heading:   headingFloat,
-				timestamp: time.Now().UnixNano() / int64(time.Millisecond),
+				d.lc.Debug(fmt.Sprintf("heading: %f", headingFloat))
+				d.devMap[devName].readingCond.L.Lock()
+
+				d.devMap[devName].readingsQueue = append(
+					d.devMap[devName].readingsQueue,
+					thingy52Heading{
+						heading: headingFloat,
+						timestamp: time.Now().UnixNano() /
+							int64(time.Millisecond),
+					},
+				)
+
+				d.devMap[devName].readingCond.Broadcast()
+				d.devMap[devName].readingCond.L.Unlock()
 			})
-
-			d.readingCond.Broadcast()
-			d.readingCond.L.Unlock()
-		})
 	})
 	if err != nil {
 		// return last error if there are multiple errors, or return the actual
@@ -83,33 +165,72 @@ func (d *Thingy52Driver) Initialize(lc logger.LoggingClient, asyncCh chan<- *dsM
 		if len(errRes) > 0 {
 			err = errRes[len(errRes)-1]
 		}
-		return errors.Wrapf(err, "registering notify callback for motion heading characteristic (uuid: %s) failed", motionUUID)
+		return errors.Wrapf(
+			err,
+			"registering notify callback for motion heading characteristic (uuid: %s) failed",
+			motionUUID,
+		)
 	}
 
 	// wait for the first reading before continuing to ensure that by the time
 	// we get real requests (and hence are "started") we can actually service
 	// them
-	d.readingCond.L.Lock()
-	for len(d.readingsQueue) == 0 {
-		d.readingCond.Wait()
+	d.devMap[devName].readingCond.L.Lock()
+	for len(d.devMap[devName].readingsQueue) == 0 {
+		readingCond.Wait()
 	}
-	d.readingCond.L.Unlock()
+	d.devMap[devName].readingCond.L.Unlock()
+	return nil
+}
 
+type thingy52Heading struct {
+	heading   float32
+	timestamp int64
+}
+
+// Initialize currently saves the logging client and notifies the driver
+// that the initialization sequence has begun, then waits to finish
+// initialization until the driver is notified that all devices have been
+// registered
+// must be run in different goroutine than FinishedRegisteringDevices and a
+// different goroutine than WaitForInitStarted (though those two functions can
+// themselves be run in the sam goroutine as each other)
+func (d *Thingy52Driver) Initialize(
+	lc logger.LoggingClient,
+	asyncCh chan<- *dsModels.AsyncValues,
+) error {
+	d.lc = lc
+	// send a struct to inform that Initialize has been called
+	d.initChan <- struct{}{}
+	// now wait for all the devices to be registered
+	d.waitForDevicesRegistered(context.TODO())
 	return nil
 }
 
 // HandleReadCommands handles read commands from the device SDK
+// Currently only supports a single request at a time
 func (d *Thingy52Driver) HandleReadCommands(
 	deviceName string,
 	protocols map[string]contract.ProtocolProperties,
 	reqs []dsModels.CommandRequest,
 ) (res []*dsModels.CommandValue, err error) {
-	// if the device isn't connected fail immediately
-	if !d.Dev.IsConnected() {
+
+	// get the device from the driver's map
+	dev, ok := d.devMap[deviceName]
+	if !ok {
+		d.lc.Warn(fmt.Sprintf("unknown device %s", deviceName))
+		err = fmt.Errorf("Thingy52Driver.HandleReadCommands; device %s does not exist in the BLE device map", deviceName)
+		return
+	}
+
+	// check if the device is connected
+	if !dev.bleDev.IsConnected() {
+		d.lc.Warn(fmt.Sprintf("device %s is not connected", deviceName))
 		err = fmt.Errorf("Thingy52Driver.HandleReadCommands; device is not connected")
 		return
 	}
 
+	// only support a single read request for now
 	if len(reqs) != 1 {
 		err = fmt.Errorf("Thingy52Driver.HandleReadCommands; too many command requests; only one supported")
 		return
@@ -122,19 +243,19 @@ func (d *Thingy52Driver) HandleReadCommands(
 
 	// acquire the lock for reading from the queue and wait until the queue
 	// has some values before processing them
-	d.readingCond.L.Lock()
-	for len(d.readingsQueue) == 0 {
-		d.readingCond.Wait()
+	dev.readingCond.L.Lock()
+	for len(dev.readingsQueue) == 0 {
+		dev.readingCond.Wait()
 	}
-	defer d.readingCond.L.Unlock()
+	defer dev.readingCond.L.Unlock()
 
 	// for this single read command, get all of the values from the queue and
 	// average them
 	// this results in smoother data when the frequency of data being created
 	// by the thingy52 is higher than EdgeX can read it (which is currently max of 1hz)
 	res = make([]*dsModels.CommandValue, 1)
-	numReadings := float64(len(d.readingsQueue))
-	lastReadingTime := d.readingsQueue[len(d.readingsQueue)-1].timestamp
+	numReadings := float64(len(dev.readingsQueue))
+	lastReadingTime := dev.readingsQueue[len(dev.readingsQueue)-1].timestamp
 
 	// we need to calculate the circular mean of the headings here, as if
 	// we have 2 headings at 0 and 359, then the average is really around 359.5
@@ -145,7 +266,7 @@ func (d *Thingy52Driver) HandleReadCommands(
 
 	sinAngleSum := float64(0.0)
 	cosAngleSum := float64(0.0)
-	for _, reading := range d.readingsQueue {
+	for _, reading := range dev.readingsQueue {
 		// turn the heading into a radian angle
 		angle := float64(reading.heading) * math.Pi / 180.0
 		// get the sine and cosine and add those to the running sums
@@ -162,38 +283,67 @@ func (d *Thingy52Driver) HandleReadCommands(
 	// then convert the degree in the range [-180,180] to [0,360] by adding
 	// 360 and taking the mod of 360 so for example mod(-90+360,360) => 270,
 	// and mod(100+360,360) == 100
-	circularMean := float32(math.Mod(math.Atan2(sinAngleMean, cosAngleMean)/math.Pi*180.0+360.0, 360.0))
+	circularMean := float32(math.Mod(
+		math.Atan2(sinAngleMean, cosAngleMean)/math.Pi*180.0+360.0,
+		360.0,
+	))
 
 	// reset the queue
-	d.readingsQueue = make([]thingy52Heading, 0)
+	dev.readingsQueue = make([]thingy52Heading, 0)
 
 	// create the command value for this request
-	cv, _ := dsModels.NewFloat32Value(reqs[0].DeviceResourceName, lastReadingTime, circularMean)
+	cv, _ := dsModels.NewFloat32Value(
+		reqs[0].DeviceResourceName,
+		lastReadingTime,
+		circularMean,
+	)
 	res[0] = cv
 
 	// turn the average unix time back into a real time.Time that we can print
 	// off for debugging
-	unixTime := float64(lastReadingTime) / float64(time.Second/time.Millisecond)
+	unixTime := float64(lastReadingTime) /
+		float64(time.Second/time.Millisecond)
 	unixTimeSec := math.Floor(unixTime)
-	unixTimeNSec := int64((unixTime - unixTimeSec) * float64(time.Second/time.Nanosecond))
+	unixTimeNSec := int64((unixTime - unixTimeSec) *
+		float64(time.Second/time.Nanosecond))
 	readingTime := time.Unix(int64(unixTimeSec), unixTimeNSec)
 
-	d.lc.Debug(fmt.Sprintf("Thingy52Driver.HandleReadCommands: avg heading = %f @ t = %v took %v", circularMean, readingTime, time.Since(start)))
+	d.lc.Debug(fmt.Sprintf(
+		"Thingy52Driver.HandleReadCommands: avg heading = %f @ t = %v took %v",
+		circularMean,
+		readingTime,
+		time.Since(start)),
+	)
 	return res, nil
 }
 
 // HandleWriteCommands does nothing becuase there are no write commands for
 // this device-service
-func (d *Thingy52Driver) HandleWriteCommands(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest, params []*dsModels.CommandValue) error {
+func (d *Thingy52Driver) HandleWriteCommands(
+	deviceName string,
+	protocols map[string]contract.ProtocolProperties,
+	reqs []dsModels.CommandRequest,
+	params []*dsModels.CommandValue,
+) error {
 	d.lc.Info(fmt.Sprintf("Thingy52Driver.HandleWriteCommands: device-thingy52 handling write commands for device %s", deviceName))
 	return nil
 }
 
 // Stop will kill the background process reading from the program
-func (d *Thingy52Driver) Stop(force bool) error {
-	d.lc.Info("Thingy52Driver.Stop: device-thingy52 driver is stopping...")
-	// try to disconnect the device and then trigger cleanup
-	err := d.Dev.Disconnect()
-	d.InterruptChan <- os.Interrupt
-	return err
+func (d *Thingy52Driver) Stop(force bool) (err error) {
+	// Stop could get called before Initialize, when we recieve our logger,
+	// so check if it's nil before using it
+	if d.lc != nil {
+		d.lc.Info("Thingy52Driver.Stop: device-thingy52 driver is stopping...")
+	}
+
+	// try to disconnect the devices and then trigger cleanup
+	for _, dev := range d.devMap {
+		// TODO more elegantly handle the case where one device disconnect
+		// fails but other devices didn't
+		// currently if the first device fails to disconnect, but the last
+		// one succeeds no error is returned
+		err = dev.bleDev.Disconnect()
+	}
+	return
 }
